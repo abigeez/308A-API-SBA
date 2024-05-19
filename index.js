be7035e91e9e3000e777ea797f1ad51d6e255162
Abi Garner <abigail.gee@hotmail.com>
2024-05-19 17:08:00 -0400

    adding baseurl

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 
 
-const 
+const API_KEY = 'live_9rACe49g2hV7l9Q8skb7kGEFeYdCzFlnfJzahqnthE095x3YCq4BituWKtglYZN';
+
+import axios from "axios";
+
+const client = axios.create({
+    baseURL:"https://api.thedogapi.com/v1/images/";
+
+});
